d8e087c3b1564bf3a1fedc3f7f74dc6d00386649
Liam Bowen <LiamBowen@gmail.com>
2014-06-14 02:39:24 -0400

    Helper routines for getting data from tumblr API

---
+++ tumblr.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"regexp"
+	"fmt"
+	"strconv"
+)
+
+var (
+	tumblrDataRegexp *regexp.Regexp =  regexp.MustCompile("http://([^/]+)/post/(\\d+)(?:/.+)?")
+)
+
+// http://lacecard.tumblr.com/post/76803575816/emacs-in-tron -> (lacecard.tumblr.com, 76803575816)
+func extractPostId(u string) (string, int, error) {
+	m := tumblrDataRegexp.FindStringSubmatch(u)
+	if m == nil {
+		return "", 0, fmt.Errorf("Could not extract base hostname and post ID")
+	}
+	bh := m[1]
+	pid, err := strconv.Atoi(m[2])
+	ensureNil(err)
+	return bh, pid, nil
+}
+
+
+func usersLikingPost(baseHostname string, postId int64) {
+
+}
