0b4190c94f5a2282e01cbb864c0e8c9128e62d27
Liam Bowen <LiamBowen@gmail.com>
2014-06-14 03:26:58 -0400

    Use 64 bit integers for post ids always

--- tumblr.go
+++ tumblr.go
@@ -11,14 +11,14 @@ var (
 )
 
 // http://lacecard.tumblr.com/post/76803575816/emacs-in-tron -> (lacecard.tumblr.com, 76803575816)
-func extractPostId(u string) (string, int, error) {
+func extractPostId(u string) (string, int64, error) {
 	m := tumblrDataRegexp.FindStringSubmatch(u)
 	if m == nil {
 		return "", 0, fmt.Errorf("Could not extract base hostname and post ID")
 	}
 	bh := m[1]
-	pid, err := strconv.Atoi(m[2])
-	ensureNil(err)
+	pid, err := strconv.ParseInt(m[2], 10, 64)
+	ensureNil(err) // For 2^64+
 	return bh, pid, nil
 }
 
